0af659dbe14c68af254dc9f0d93f84e209cab95f
ForeverEmpty_ <2829951243@qq.com>
2026-05-07 16:39:40 +0800

    feat: add page transition animations

--- src/layouts/index.vue
+++ src/layouts/index.vue
@@ -7,8 +7,12 @@ import Header from './Header/index.vue'
     class="min-h-screen w-full bg-bg-main text-text-main font-sans transition-colors duration-700 selection:bg-selection"
   >
     <Header />
-    <main class="mx-auto flex h-screen w-full justify-center pt-24">
-      <RouterView />
+    <main class="mx-auto flex h-screen w-full justify-center overflow-hidden pt-24">
+      <RouterView v-slot="{ Component, route }">
+        <Transition name="page-route" mode="out-in" appear>
+          <component :is="Component" :key="route.fullPath" />
+        </Transition>
+      </RouterView>
     </main>
   </div>
 </template>
